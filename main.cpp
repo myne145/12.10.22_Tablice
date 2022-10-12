6401bae537d02e4873788b3b733e0462af27abe0
myne145 <antoni.kucz@gmail.com>
2022-10-12 10:08:34 +0200

    V2

--- main.cpp
+++ main.cpp
@@ -5,11 +5,9 @@ using namespace std;
 
 int main() {
     int array[10];
-    for(int i = 0; i < sizeof(array) / sizeof(int); i++) {
-        array[i] = rand();
-    }
     cout << "Parzyste" << endl;
     for(int i = 0; i < sizeof(array) / sizeof(int); i++) {
+        array[i] = rand();
         if(array[i] % 2 == 0) {
             cout << array[i];
             cout << " ";
